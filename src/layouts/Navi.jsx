73fcab22bf359f9b5b1f53c6f54326e33e092918
Ayşe Firdevs Çelik <50719300+aysefirdevs@users.noreply.github.com>
2021-06-10 00:15:45 +0300

    10 haziran son

--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -3,12 +3,15 @@ import CartSummary from './CartSummary'
 import {Container, Menu } from 'semantic-ui-react'
 import SignedOut from './SignedOut'
 import SignedIn from './SignedIn'
+import { useHistory } from 'react-router'
 
 export default function Navi() {
     const [isAuthenticated, setIsAuthenticated] = useState(true)
+    const history= useHistory()
 
     function handleSignOut(params){  //çıkış yapı handle ediyorum
         setIsAuthenticated(false)
+        history.push("/") //çıkış yaptığında sepet detayında olmak yerine anasayfaya yönlendiriyor.
     }
 
     function handleSignIn(params){  //giriş yapı handle ediyorum
